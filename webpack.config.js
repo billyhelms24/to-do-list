13662caa93bba0cfb6c8473894423511b7ad9626
billyhelms24 <williamtvhelms@gmail.com>
2021-04-16 11:19:16 -0400

    Reconfigure for asset management

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     entry: {
         index: "./src/index.js",
         taskManager: "./src/taskManager.js",
+        displayController: "./src/displayController.js",
     },
     devtool: "inline-source-map",
     devServer: {
